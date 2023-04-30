b8045927eeec4fbd301f23553859c63ae66c962d
Harsh Parakh <85981083+Harsh-parakh22@users.noreply.github.com>
2023-04-30 19:36:52 +0530

    Add files via upload

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,12 @@
+const FirebaseConfig = {
+  apiKey: "AIzaSyBlrCvhQ317r5tBRCGt2uGcqkkYPW2MXC8",
+  authDomain: "crypto-creed-a0c31.firebaseapp.com",
+  projectId: "crypto-creed-a0c31",
+  storageBucket: "crypto-creed-a0c31.appspot.com",
+  messagingSenderId: "321016599730",
+  appId: "1:321016599730:web:a9b972c017fd6554c725ab",
+  measurementId: "G-TTGF1F9BVW"
+};
+
+export default FirebaseConfig;
+// Made with ♥ by Harsh Parakh
