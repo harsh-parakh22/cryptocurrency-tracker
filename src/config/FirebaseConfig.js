9d822bd3de2488462577feadd8177429a03a30e2
Harsh Parakh <85981083+harsh-parakh22@users.noreply.github.com>
2025-09-06 15:37:45 +0530

    Update FirebaseConfig.js

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,12 +1,13 @@
 const FirebaseConfig = {
-  apiKey: "AIzaSyBlrCvhQ317r5tBRCGt2uGcqkkYPW2MXC8",
-  authDomain: "crypto-creed-a0c31.firebaseapp.com",
-  projectId: "crypto-creed-a0c31",
-  storageBucket: "crypto-creed-a0c31.appspot.com",
-  messagingSenderId: "321016599730",
-  appId: "1:321016599730:web:a9b972c017fd6554c725ab",
-  measurementId: "G-TTGF1F9BVW"
+  apiKey: "<api_key>",
+  authDomain: "<key>",
+  projectId: "<id>",
+  storageBucket: "<key>,
+  messagingSenderId: "<id>",
+  appId: "<app_id>",
+  measurementId: "<id>"
 };
 
 export default FirebaseConfig;
+
 // Made with ♥ by Harsh Parakh
